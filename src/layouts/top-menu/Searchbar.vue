1ce7688f92750d248890e8974cea99059de22a37
Nino Gralla <nino.gralla@gmx.de>
2021-09-02 01:44:14 +0200

    Translate Topmenu - Searchbox (EN)

--- src/layouts/top-menu/Searchbar.vue
+++ src/layouts/top-menu/Searchbar.vue
@@ -6,7 +6,7 @@
         <input
           type="text"
           class="search__input form-control dark:bg-dark-1 border-transparent placeholder-theme-13"
-          placeholder="Search..."
+          :placeholder="$t('utils.search')"
           @focus="showSearchDropdown"
           @blur="hideSearchDropdown"
           v-model="keywords"
@@ -24,14 +24,14 @@
         <div class="intro-y col-span-12 sm:col-span-12 md:col-span-12 xxl:col-span-12" v-if="this.search_results.cats.length === 0 && this.search_results.posts.length === 0 && this.search_results.users.length === 0">
           <div class="relative flex items-center">
             <div class="p-1">
-              <div class="text-3xl">Nothing found</div>
+              <div class="text-3xl">{{ $t('topbar.search_modal_no_result') }}</div>
               <div class="text-gray-600 mt-2">
-                Please select another Keyword to search.
+                {{ $t('topbar.select_other_query') }}
               </div>
             </div>
           </div>
         </div>
-        <div class="search-result__content__title" v-if="this.search_results.cats.length > 0">Categories</div>
+        <div class="search-result__content__title" v-if="this.search_results.cats.length > 0">{{ $t('topbar.filter_categories') }}</div>
         <div class="mb-5">
           <a
             v-for="result in this.search_results.cats.slice(0, 5)"
@@ -53,7 +53,7 @@
             </div>
           </a>
         </div>
-        <div class="search-result__content__title" v-if="this.search_results.posts.length > 0">Posts</div>
+        <div class="search-result__content__title" v-if="this.search_results.posts.length > 0">{{ $t('topbar.filter_posts') }}</div>
         <div class="mb-5">
           <a
             v-for="result in this.search_results.posts.slice(0, 5)"
@@ -73,7 +73,7 @@
             <div class="ml-auto w-48 truncate text-gray-600 text-xs text-right" v-html="result.content.substring(0,20)"></div>
           </a>
         </div>
-        <div class="search-result__content__title" v-if="this.search_results.users.length > 0">Authors</div>
+        <div class="search-result__content__title" v-if="this.search_results.users.length > 0">{{ $t('topbar.filter_authors') }}</div>
         <div class="mb-5">
           <a
             v-for="result in this.search_results.users.slice(0, 5)"
@@ -96,7 +96,8 @@
         </div>
         <router-link :to="{ name: 'search' }">
           <button class="btn btn-primary mr-2 mb-2">
-            <SearchIcon class="w-4 h-4 mr-2" /> View Searchpage
+            <SearchIcon class="w-4 h-4 mr-2" />
+            {{ $t('topbar.view_searchpage') }}
           </button>
         </router-link>
       </div>
